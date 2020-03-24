dbc4c1d08692e89acc34ab79fbf81ca2c257319a
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-03-24 20:12:56 +0000

    punto 1

---
+++ HTTPRequestVerifica/src/app/driving.model.ts
@@ -0,0 +1,9 @@
+export class DrivingModel{
+
+    constructor(
+        public tipo: string,
+        public descrizione: string,
+        public tariffa: string,
+        public valutazionemedia: string,
+    ){}
+}
